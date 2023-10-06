3580b50fa119b3e9f66113ac9ea17ca20fbc672a
kazungu <kazungu61@outlook.com>
2023-10-06 20:08:04 +0300

    Variables and comments

---
+++ variables_comments.py
@@ -0,0 +1,18 @@
+#This is comment. It will be ignored
+#variables in python
+#assign variables
+first_name = 'Francis'
+last_name = 'Kiarie'
+age = 29
+variableExample1 = 'I love Philosophy'
+
+#print variables
+print(variableExample1)
+#concatination examples
+print('I am '+ first_name + "" + last_name)
+print(f"I am {age} years old")
+
+#check variable type
+
+print(type(first_name))
+print(type(variableExample1) == str) #Retrun True if variable type is string
