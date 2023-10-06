140b35543bfd596f02e9983d2a33c14f638bb882
kazungu <kazungu61@outlook.com>
2023-10-06 20:39:01 +0300

    arithmetic operations

---
+++ arithmetic_operaters.py
@@ -0,0 +1,55 @@
+#Arithmetic operators
+
+#Addition
+print(5+5)
+
+num1, num2 = 6, 8
+total = num1 + num2
+print(total)
+
+#subtraction
+print(100-76)
+
+number1, number2 = 300, 45
+result = number1 + number2
+print(result)
+
+#multiplication
+print(5*5)
+
+sample1, sample2 = 30,5
+product= sample1*sample2
+
+#division
+print(200 / 10)
+
+sample3, sample4 = 500 , 10
+div_result = sample3 / sample4
+print(div_result)
+
+#modulus
+print(200 % 9)
+
+sample5, sample6 = 300, 13
+m_result = sample5 % sample6
+print(m_result)
+
+#integer division, round up to the nearest interger. 
+print(346 // 36)
+
+sample7,sample8 = 455, 32
+end_result = sample7 // sample8
+print(end_result)
+
+#Exponentiation
+print(2 ** 3)
+
+sample9, sample10 = 4, 3
+e_result = sample9 ** sample10
+print(e_result)
+
+#Negation
+x = 10
+neg_x = -x
+print(neg_x)  # Output: -10
+
