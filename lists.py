167fb2740bc31c92c9fd43729f1552e3ea13f7ac
kazungu <kazungu61@outlook.com>
2023-12-09 19:22:42 +0300

    Add list slicing

--- lists.py
+++ lists.py
@@ -1,6 +1,21 @@
-a = [1,2,3,4]
-b= [5,6,7,8]
+# concatinate
+a = [1, 2, 3, 4]
+b = [5, 6, 7, 8]
 
-c= a+b
+c = a + b
 
 print(c)
+
+# list sclicing
+
+t = [4, 19, 45, 67, 34, 78, 90, 100, 112]
+slice1 = t[1:3]  # up to but not including index 3
+print(slice1)
+
+slice2 = t[:4]  # up to but not including index 4
+
+print(slice2)
+
+slice3 = t[3:]  # index 3 to the end.
+
+print(slice3)
