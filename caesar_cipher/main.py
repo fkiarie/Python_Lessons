35ac732e5462db418f152308cc012ac2976ec539
kazungu <kazungu61@outlook.com>
2024-01-21 13:14:20 +0300

    define function vegenere and local variables

--- caesar_cipher/main.py
+++ caesar_cipher/main.py
@@ -4,3 +4,7 @@ Implementing a vignere cipher encryption and decryption
 text = 'mrttaqrhknsw ih puggrur'
 custom_key = 'python'
 
+def vegenere(message, key, direction=1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_text = ''
