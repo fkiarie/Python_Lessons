9d90ab877e99e3373ae9124e67f4143d59b27b52
kazungu <kazungu61@outlook.com>
2024-01-21 13:41:19 +0300

    encryption and decryption logic

--- caesar_cipher/main.py
+++ caesar_cipher/main.py
@@ -8,3 +8,22 @@ def vegenere(message, key, direction=1):
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     final_text = ''
+
+    for char in message.lower():
+
+        # Append any non-character to message
+        if not char.isalpha():
+            final_text += char
+        else:
+            #Find the right key character to encode/decrypt
+            key_char = key[key_index % len(key)]
+            key_index +=1
+
+            # Define the offset and the encrypted/decrypted letter
+
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_text += alphabet[new_index]
+    
+    return final_text
