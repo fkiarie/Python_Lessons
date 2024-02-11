1795bc5e666dff20ff437437b0dc97300f935c2f
kazungu <kazungu61@outlook.com>
2024-02-11 18:30:49 +0300

    set questions

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,32 @@
 print("Welcome to my game!")
+
+playing = input("Do you want to play a game? ")
+
+if playing != "yes":
+    quit()
+
+print("Let's play!")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("correct!")
+else:
+    print("incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer == "graphics processing unit":
+    print("correct!")
+else:
+    print("incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer == "random access memory":
+    print("correct!")
+else:
+    print("incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer == "power supply unit":
+    print("correct!")
+else:
+    print("incorrect!")
