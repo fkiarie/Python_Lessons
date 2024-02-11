001d515ee4ef8252f1bed1d97bc329abd1d4ec24
kazungu <kazungu61@outlook.com>
2024-02-11 05:22:39 +0300

    Card number transaltion

--- luhn_algorithim/main.py
+++ luhn_algorithim/main.py
@@ -4,4 +4,5 @@ implement Luhn Algorithim
 def main():
     card_number = '4111-1111-4555-1141'
     card_translation = str.maketrans({'-':'', ' ': ''})
+    translated_card_number = card_number.translate(card_translation)
     
