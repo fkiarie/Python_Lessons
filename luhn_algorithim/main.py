53fadfef2c637abb529eb360051ef40f9a67110b
kazungu <kazungu61@outlook.com>
2024-02-11 05:28:54 +0300

    get odd digits

--- luhn_algorithim/main.py
+++ luhn_algorithim/main.py
@@ -1,8 +1,13 @@
 """
 implement Luhn Algorithim
 """
-def verify_card_number():
-    pass
+def verify_card_number(card_number):
+    sum_of_odd_digits = 0
+    card_number_reversed = card_number[::-1]
+    odd_digits = card_number_reversed[::2]
+
+    for digits in odd_digits:
+        sum_of_odd_digits += int(digits)
 def main():
     card_number = '4111-1111-4555-1141'
     card_translation = str.maketrans({'-':'', ' ': ''})
