9bc7c04d12003ea10690927972b2047b7bd27825
kazungu <kazungu61@outlook.com>
2024-02-11 05:25:28 +0300

    Create function to verify card number

--- luhn_algorithim/main.py
+++ luhn_algorithim/main.py
@@ -1,8 +1,10 @@
 """
 implement Luhn Algorithim
 """
+def verify_card_number():
+    pass
 def main():
     card_number = '4111-1111-4555-1141'
     card_translation = str.maketrans({'-':'', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
-    
+    verify_card_number()
