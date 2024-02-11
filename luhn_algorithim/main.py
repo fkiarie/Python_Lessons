271350316cd3794522075b491ff3c1cd81abf1cf
kazungu <kazungu61@outlook.com>
2024-02-11 05:48:41 +0300

    call verify carnumber function and check for valid card numbers

--- luhn_algorithim/main.py
+++ luhn_algorithim/main.py
@@ -27,6 +27,9 @@ def main():
     card_number = '4111-1111-4555-1141'
     card_translation = str.maketrans({'-':'', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
-    verify_card_number(card_number)
+    if verify_card_number(translated_card_number):
+        print("VALID!")
+    else:
+        print("INVALID!")
 
 main()
