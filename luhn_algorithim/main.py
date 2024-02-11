b5efbe541e047f6e1868ab3fb4e1b919c8db5ad3
kazungu <kazungu61@outlook.com>
2024-02-11 05:44:16 +0300

    get even digits

--- luhn_algorithim/main.py
+++ luhn_algorithim/main.py
@@ -8,8 +8,25 @@ def verify_card_number(card_number):
 
     for digits in odd_digits:
         sum_of_odd_digits += int(digits)
+    
+    sum_of_even_digits = 0
+    even_digits = card_number_reversed[1::2]
+    
+    for digit in even_digits:
+        number = int(digit) *2
+        if number >=10:
+            number = (number // 10) + (number % 10)
+        
+        sum_of_even_digits += number
+    
+    total = sum_of_odd_digits + sum_of_even_digits
+    return total % 10 == 0
+
+    
 def main():
     card_number = '4111-1111-4555-1141'
     card_translation = str.maketrans({'-':'', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
-    verify_card_number()
+    verify_card_number(card_number)
+
+main()
