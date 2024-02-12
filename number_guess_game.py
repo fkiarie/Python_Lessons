2b154822e33b6c3ac1d6b3f4ce0ed6a7fc872ea1
kazungu <kazungu61@outlook.com>
2024-02-12 06:23:06 +0300

    add user guesing part

--- number_guess_game.py
+++ number_guess_game.py
@@ -6,11 +6,26 @@ top_of_range = input("Type a number: ")
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
 
-    if top_of_range <=0:
+    if top_of_range <= 0:
         print("Enter number larger than 0")
         quit()
 else:
     print("Enter number please")
     quit()
 
-random_number = random.randrange(-2, 11)
+random_number = random.randint(0,top_of_range)
+
+while True:
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Type a number")
+        continue
+
+    if user_guess == random_number:
+        print("You got it")
+    else:
+        print("you got it wrong")
+
+
