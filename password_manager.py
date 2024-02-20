d439f1b2cca4405986592612bbe41965aca1f947
kazungu <kazungu61@outlook.com>
2024-02-20 13:39:02 +0300

    while loop to check user input

--- password_manager.py
+++ password_manager.py
@@ -1,3 +1,14 @@
 pwd = input("Enter the master password")
 
-mode = input("View or add new password? (view, add) ")
+while True:
+    mode = input("View or add new password? (view, add) press q for quit").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    else:
+        print("Invalid input")
+        continue
