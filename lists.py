9e0a606025ede76e229b89d66002eea834dbd7a9
kazungu <kazungu61@outlook.com>
2024-03-01 06:13:17 +0300

    improve output

--- lists.py
+++ lists.py
@@ -4,17 +4,17 @@ b = [5, 6, 7, 8]
 
 c = a + b
 
-print(c)
+print("concatinated", c)
 
 # list sclicing
 
 t = [4, 19, 45, 67, 34, 78, 90, 100, 112]
 slice1 = t[1:3]  # up to but not including index 3
-print(slice1)
+print("slice 1:", slice1)
 
 slice2 = t[:4]  # up to but not including index 4
 
-print(slice2)
+print("Slice 2:", slice2)
 
 slice3 = t[3:]  # index 3 to the end.
 
