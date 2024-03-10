9508de64515b302bdb1384a29833bdebca8de683
kazungu <kazungu61@outlook.com>
2024-03-10 14:43:48 +0300

    filter by category logic

--- expense_tracker.py
+++ expense_tracker.py
@@ -6,3 +6,6 @@ def print_expenses(expenses):
         print(f'Amount: {expenses["amount"]}, Category {expenses["category"]}')
 def total_expenses(expenses):
     return sum(map(lambda expense: expense['amount'], expenses))
+def filter_expenses_by_category(expenses, category):
+    return filter(lambda expense: expense['category'] == category, expenses)
+
