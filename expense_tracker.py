12e7581b34e2f71561260c44bb523ac64d8e2052
kazungu <kazungu61@outlook.com>
2024-03-10 14:40:41 +0300

    total expenses logic

--- expense_tracker.py
+++ expense_tracker.py
@@ -4,3 +4,5 @@ def add_expenses(expenses, amount, category):
 def print_expenses(expenses):
     for expense in expenses:
         print(f'Amount: {expenses["amount"]}, Category {expenses["category"]}')
+def total_expenses(expenses):
+    return sum(map(lambda expense: expense['amount'], expenses))
